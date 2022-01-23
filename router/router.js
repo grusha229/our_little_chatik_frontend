2293a43c8a1ce4f6159e7762897a3fe371738563
Grigory Tupikov <gtupikov@kabanov.agency>
2022-01-23 23:36:57 +0300

    initial

---
+++ router/router.js
@@ -0,0 +1,20 @@
+export class router {
+    constructor() {
+        this.routs = {
+            '/': displayMessages,
+            '/signup': authModule.renderRegistration,
+            '/login': authModule.renderAuth,
+            '/logout': logOut,
+        }
+    }
+
+    goTo(path, title, state=null) {
+        state.path = path;
+        state.title = title;
+        window.history.pushState(
+            state, // объект состояния
+            state.title, // заголовок состояния
+            path, // URL новой записи (same origin)
+        );
+    }
+}
