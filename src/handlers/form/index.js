ff3fbd35333d2284812ac2dc034f9e103a27e979
Grigory Tupikov <gtupikov@kabanov.agency>
2022-02-18 00:11:51 +0300

    add fetching v0.2.2, it doesnt work for now :(

--- src/handlers/form/index.js
+++ src/handlers/form/index.js
@@ -3,51 +3,55 @@ import {fetchRequest} from "../network/fetch";
 import {Router} from "../../../router/router";
 
 // обработка отправки формы
-const form = document.getElementById('form');
+export function formHandler() {
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert("gogogo");
-    debugger;
-    const firstname = document.getElementById('firstname').value;
-    const lastname = document.getElementById('lastname').value;
-    const password = document.getElementById('password').value;
-    const username = document.getElementById('username').value;
-    // let msg = '';
-    // if (!validators.username(name)) {
-    //     msg += 'Имя должно быть длиннее 3 символов. ';
-    // }
-    // if (!validators.password(pwd)) {
-    //     msg += 'Пароль должен быть от 6 до 16 символов. ';
-    // }
-    // if (msg !== '') {
-    //     showErrors(msg );
-    // } else {
-    const user = {
-        firstname: firstname,
-        lastname: lastname,
-        password: password,
-        username: username
-    };
-    alert(user);
-    const url = serverLocate+'/auth/signup';
-    // 3.67.182.34
-    const data = JSON.stringify(user);
-    alert(data);
-    debugger;
-    fetchRequest(url, 'POST', data).then((result)=>{
-        if (!result.ok) {
-            alert('error - unlogined');
-            throw error;
-        }
-    }).then(
-        () => {
-            alert('Logined');
-            // Router.goTo('/', '', null, true, true);
-        },
-    ).catch(function() {
-        alert('Неверный логин или пароль');
-        // showErrors('Неверный логин или пароль');
+    const form = document.getElementById('form');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        console.log("gogogo");
+        debugger;
+        const firstname = document.getElementById('firstname').value;
+        const lastname = document.getElementById('lastname').value;
+        const password = document.getElementById('password').value;
+        const username = document.getElementById('username').value;
+        // let msg = '';
+        // if (!validators.username(name)) {
+        //     msg += 'Имя должно быть длиннее 3 символов. ';
+        // }
+        // if (!validators.password(pwd)) {
+        //     msg += 'Пароль должен быть от 6 до 16 символов. ';
+        // }
+        // if (msg !== '') {
+        //     showErrors(msg );
+        // } else {
+        const user = {
+            firstname: firstname,
+            lastname: lastname,
+            password: password,
+            username: username
+        };
+        console.log(user);
+        const url = serverLocate + '/auth/signup';
+        // 3.67.182.34
+        const data = JSON.stringify(user);
+        console.log(data);
+        debugger;
+        fetchRequest(url, 'POST', data).then((result) => {
+            console.log(result);
+            if (!result.ok) {
+                alert('error - unlogined');
+                throw error;
+            }
+        }).then(
+            () => {
+                alert('Logined');
+                // Router.goTo('/', '', null, true, true);
+            },
+        ).catch(function () {
+            alert('Неверный логин или пароль');
+            // showErrors('Неверный логин или пароль');
+        });
+        // }
     });
-    // }
-});
+}
